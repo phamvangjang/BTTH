f35132bea4d90568cab7450cbe42f79e1c85982e
phamvangjang <giangphamvan48@gmail.com>
2023-12-09 14:48:30 +0700

    update form register

--- Lab03_3/Models/Country.cs
+++ Lab03_3/Models/Country.cs
@@ -5,9 +5,9 @@ using System.Web;
 
 namespace Lab03_3.Models
 {
-    public class LoaiHinh
+    public class Country
     {
         public string ID { get; set; }
-        public string TenLoaiHinh { get; set; }
+        public string Name { get; set; }
     }
 }
